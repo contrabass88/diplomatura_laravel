51d7c7614af29682fd6483bb73c92ffd2fd1bf22
Gaston <gastonrodriguez401@gmail.com>
2025-05-12 00:55:43 -0300

    migracion de base de datos

--- app/Models/Provincia.php
+++ app/Models/Provincia.php
@@ -9,11 +9,11 @@ class Provincia extends Model
 {
     use SoftDeletes;
 
-    protected $table = 'provincias';
+    protected $table = 'provincias'; /* el modelo provincia se va a relacionar con la tabla provincia que se crea en la BD*/
     protected $primaryKey = 'id';
-    public $incrementing = true;
+    public $incrementing = true; /* el id va a ser autoincremental*/
 
-    protected $fillable = [
+    protected $fillable = [  /*atributo es un array donde se coloca todos los campos que tiene una tabla menos el id*/
         'nombre',
         'codigo'
     ];
