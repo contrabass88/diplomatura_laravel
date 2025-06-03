45d48d00758ac4a644d76bca9d9d4c3a556d9397
Gaston <gastonrodriguez401@gmail.com>
2025-06-03 15:20:08 -0300

    Login de usuario con token Sanctum y validaciones

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,19 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProvinciaController;
 use App\Http\Controllers\TiketController;
+use App\Http\Controllers\API\AuthController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/get-provincias', [ProvinciaController::class, 'index']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/get-provincias', [ProvinciaController::class, 'index']);
+});
+
 Route::get('/get-provincia-tiket/{id}', [TiketController::class, 'getProvinciaTikets']);
 Route::get('provincias-tikets', [ProvinciaController::class, 'provinciasConTikets']);
 
@@ -20,4 +26,3 @@ Route::get('get-tikets/{id}', [TiketController::class, 'show']);
 Route::post('set-tikets', [TiketController::class, 'store']);
 Route::put('update-tikets/{id}', [TiketController::class, 'update']);
 Route::delete('delete-tikets/{tiket}', [TiketController::class, 'destroy']);
-
